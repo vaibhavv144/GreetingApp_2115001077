344339cebfc2e20f4e8358464559f56427b3e384
Vaibhav <vaibhavsinggh144@gmail.com>
2025-03-04 23:52:49 +0530

    [Vaibhav] Add. Adding UC1 codes

---
+++ BusinessLayer/Services/GreetingBL.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer.Interface;
+
+namespace BusinessLayer.Services
+{
+    public class GreetingBL:IGreetingBL
+    {
+    }
+}
---
+++ HelloGreetingApplication/Program.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Web;
+using BusinessLayer.Interface;
+using BusinessLayer.Services;
+using RepositoryLayer.Interface;
+using RepositoryLayer.Services;
+using Microsoft.OpenApi.Models;
+
+var logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+logger.Info("Starting up the application");
+
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Load NLog configuration
+    builder.Logging.ClearProviders();
+    builder.Host.UseNLog();
+
+    // Add services to the container.
+    builder.Services.AddControllers();
+
+
+    // Configure Swagger
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "User Registration API", Version = "v1" });
+    });
+
+    var app = builder.Build();
+
+    // Enable Swagger UI
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseDeveloperExceptionPage();
+        app.UseSwagger();
+        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "User Registration API v1"));
+    }
+
+    // Configure the HTTP request pipeline
+    app.UseHttpsRedirection();
+    app.UseAuthorization();
+    app.MapControllers();
+
+    app.Run();
+}
+catch (Exception ex)
+{
+    logger.Error(ex, "Application startup failed.");
+    throw;
+}
+finally
+{
+    LogManager.Shutdown();
+}
